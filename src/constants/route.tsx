5bbbad4a0fcc369e53faf16f60140b5087ce93c5
huy-vu <quochuy123dh@gmail.com>
2022-12-18 08:17:58 +0700

    #7 - Huy Introduction

--- src/constants/route.tsx
+++ src/constants/route.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
-import { HOMEPAGE_PATH } from './paths';
+import HuyIntroductionPage from '../pages/HuyIntroductionPage';
+import { HOMEPAGE_PATH, HUY_INTRO_PAGE } from './paths';
 
 const route = createBrowserRouter([
   {
     path: HOMEPAGE_PATH,
     element: <HomePage />,
+  },
+  {
+    path: HUY_INTRO_PAGE,
+    element: <HuyIntroductionPage />,
   },
 ]);
 
